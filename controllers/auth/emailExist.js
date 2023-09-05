d9269a80aef252e1a92afdfbcc3a261e4f371cd7
Devansh-gaur-1611 <devansh.gaur.iitbhu20@gmail.com>
2023-09-05 14:15:26 +0530

    username exist fxn created

--- controllers/auth/emailExist.js
+++ controllers/auth/emailExist.js
@@ -1,21 +1,50 @@
-const joi = require("joi")
-const User = require("../../models/user")
+const joi = require("joi");
+const User = require("../../models/user");
 
-const emailExist = async (req, res, next) => {
+const userCheck = {
+  async emailExist(req, res, next) {
     const schema = joi.object({
-        email: joi.string().required()
-    })
+      email: joi.string().required(),
+    });
 
     const { error } = schema.validate(req.body);
-    if(error) {
-        return next(error)
+    if (error) {
+      return next(error);
     }
 
-    const userExist = await User.findOne({email:req.body.email});
-    if(userExist){
-        return res.status(200).json({message:true});
+    try {
+      const userExist = await User.findOne({ email: req.body.email });
+      if (userExist) {
+        return res.status(200).json({ message: true });
+      }
+      return res.status(200).json({ message: false });
+    } catch (error) {
+      console.log(error.message);
+      return res.status(500).json({ error: "Internal Server Error" });
     }
-    return res.status(200).json({message:false});
-}
+  },
 
-module.exports = emailExist
+  async userNameExist(req, res, next) {
+    const schema = joi.object({
+      username: joi.string().required(),
+    });
+
+    const { error } = schema.validate(req.body);
+    if (error) {
+      return next(error);
+    }
+
+    try {
+      const userExist = await User.findOne({ userName: req.body.username });
+      if (userExist) {
+        return res.status(200).json({ message: true });
+      }
+      return res.status(200).json({ message: false });
+    } catch (error) {
+      console.log(error.message);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+  },
+};
+
+module.exports = userCheck;
